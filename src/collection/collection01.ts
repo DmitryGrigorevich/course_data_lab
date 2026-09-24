7fc9b08376aa614da50d85e6f628bdf6e07eb72a
DmitryGrigorevich <mitya_107@icloud.com>
2026-09-24 23:05:09 +0300

    collection01

--- src/collection/collection01.ts
+++ src/collection/collection01.ts
@@ -1,9 +1,13 @@
 /* 
-	Создайте генераторную функцию numberGenerator, которая принимает число max и возвращает генератор, yielding числа от 1 до max включительно.
+	Создайте генераторную функцию numberGenerator, 
+	которая принимает число max и возвращает генератор,
+	yielding числа от 1 до max включительно.
 */
 
 export function* numberGenerator(max: number) {
-
+	for (let elem = 1; elem <= max; elem++) {
+		yield elem;
+	}
 }
 
 
