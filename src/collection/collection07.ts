4ca8419c75c41a229e74b53e178a98fd0565e059
DmitryGrigorevich <mitya_107@icloud.com>
2026-09-25 18:00:58 +0300

    collection07

--- src/collection/collection07.ts
+++ src/collection/collection07.ts
@@ -1,9 +1,17 @@
 /* 
-  Напишите функцию countFrequency, которая принимает массив строк и возвращает Map, где ключи - это элементы массива, а значения - количество их вхождений.
+  Напишите функцию countFrequency, 
+  которая принимает массив строк и возвращает Map, 
+  где ключи - это элементы массива, а значения - количество их вхождений.
 */
 
 export function countFrequency(arr: string[]): Map<string, number> {
   const map = new Map();
+  
+  for (let elem of arr) {
+	if (map.has(elem)) continue;
+	let tmp: string [] = arr.filter((fillElem) => fillElem === elem)
+	map.set(elem, tmp.length)
+  }
 
   return map;
 }
