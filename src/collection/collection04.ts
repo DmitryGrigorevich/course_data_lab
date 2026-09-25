22fb41eb7de8d689302909c16177b7b957ac5b08
DmitryGrigorevich <mitya_107@icloud.com>
2026-09-25 17:36:23 +0300

    collection04

--- src/collection/collection04.ts
+++ src/collection/collection04.ts
@@ -1,9 +1,13 @@
 /* 
-	Создайте генераторную функцию filterEven, которая принимает массив чисел и возвращает только четные числа из этого массива, используя yield.
+	Создайте генераторную функцию filterEven, 
+	которая принимает массив чисел и возвращает только четные числа из этого массива, 
+	используя yield.
 */
 
 export function* filterEven(arr: number[]): IterableIterator<number> {
-
+	for (let elem of arr) {
+		if (elem % 2 == 0) yield elem;
+	}
 }
 
 
