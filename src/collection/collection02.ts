c1d175e76238a084663a616c890fbaaa3e482397
DmitryGrigorevich <mitya_107@icloud.com>
2026-09-25 17:24:19 +0300

    collection02

--- src/collection/collection02.ts
+++ src/collection/collection02.ts
@@ -1,5 +1,7 @@
 /* 
-	Реализуйте итератор для объекта obj, чтобы при использовании в цикле for...of он возвращал значения свойств объекта, а не ключи.
+	Реализуйте итератор для объекта obj, 
+	чтобы при использовании в цикле for...of он 
+	возвращал значения свойств объекта, а не ключи.
 */
 
 export const obj = {
@@ -7,7 +9,26 @@ export const obj = {
   b: 2,
   c: 3,
   [Symbol.iterator]() {
+	let count = 0;
+	let objess = [this.a, this.b, this.c]
 
+	return {
+		next():IteratorResult<number> {
+			if (count < objess.length) {
+				const tmp =  {
+					value: objess[count],
+					done: false
+				}
+				count++;
+				return tmp;
+			}
+
+			return {
+				value: undefined,
+				done: true
+			}
+		}
+	}
   }
 };
 
