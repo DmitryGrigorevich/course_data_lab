be3b50619e37968e61c0c490077c6faf5f63109e
DmitryGrigorevich <mitya_107@icloud.com>
2026-09-25 18:16:22 +0300

    collection08

--- src/collection/collection08.ts
+++ src/collection/collection08.ts
@@ -1,9 +1,15 @@
 /* 
-	Создайте функцию unionSets, которая принимает два Set и возвращает новый Set, содержащий все элементы из обоих множеств. 
+	Создайте функцию unionSets, 
+	которая принимает два Set и возвращает новый Set, 
+	содержащий все элементы из обоих множеств. 
 */
 
 export function unionSets<T>(set1: Set<T>, set2: Set<T>): Set<T> {
+	const newSet = new Set(set1)
 
+	set2.forEach(elem => newSet.add(elem))
+
+	return newSet;
 }
 
 
