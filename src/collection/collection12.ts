86ad7fbe004f581e2e14e9b645832e265f9220a4
DmitryGrigorevich <mitya_107@icloud.com>
2026-09-26 14:12:29 +0300

    collection12

--- src/collection/collection12.ts
+++ src/collection/collection12.ts
@@ -1,10 +1,20 @@
 /* 
-	Напишите функцию-обертку cache, которая кэширует результаты вызова функции с помощью Map, чтобы избежать повторных вычислений для одинаковых аргументов.
+	Напишите функцию-обертку cache, которая кэширует результаты вызова функции с помощью Map,
+	чтобы избежать повторных вычислений для одинаковых аргументов.
 */
 
 export function cache<T extends (...args: any[]) => any>(fn: T): T {
-  const cacheMap = new Map<string, any>();
-  return function(this: any, ...args: any[]) {
+	const cacheMap = new Map<string, any>();
 
-  } as T;
+	return function(this: any, ...args: any[]) {
+		const key = JSON.stringify(args);
+		
+		if (cacheMap.has(key)) return cacheMap.get(key);
+
+		const res = fn.apply(this, args);
+		cacheMap.set(key, res);
+
+		return res;
+	
+	} as T;
 }
