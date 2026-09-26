4e70585b20b25f27519f7b9c7485323e0618d5b2
DmitryGrigorevich <mitya_107@icloud.com>
2026-09-26 13:44:46 +0300

    collection11

--- src/collection/collection11.ts
+++ src/collection/collection11.ts
@@ -1,7 +1,19 @@
 /* 
-	Создайте функцию groupBy, которая принимает массив объектов и ключ, возвращает Map, где ключи - значения этого свойства, а значения - массивы объектов с таким значением свойства.
+	Создайте функцию groupBy, которая принимает массив объектов и ключ, 
+	возвращает Map, где ключи - значения этого свойства, 
+	а значения - массивы объектов с таким значением свойства.
 */
 
-export function groupBy<T extends Record<string, any>>(arr: T[], key: keyof T): Map<T[keyof T], T[]> {
+export function groupBy<T extends Record<string, any>>(
+	arr: T[], key: keyof T
+): Map<T[keyof T], T[]> {
+	const map = new Map<T[keyof T], T[]>();
 
+	arr.forEach((elem) => {
+		const value = elem[key];
+		const needArrObj = arr.filter((elem) => elem[key] === value)
+		map.set(value, needArrObj)
+	})
+
+	return map;
 }
