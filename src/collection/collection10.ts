9c35e9ff2f1727b110a7ee33778849a1e445414c
DmitryGrigorevich <mitya_107@icloud.com>
2026-09-26 12:30:03 +0300

    collection10

--- src/collection/collection10.ts
+++ src/collection/collection10.ts
@@ -1,9 +1,15 @@
 /* 
-	Создайте функцию invertMap, которая принимает Map и возвращает новый Map, где ключи и значения поменяны местами.
+	Создайте функцию invertMap, 
+	которая принимает Map и возвращает новый Map, 
+	где ключи и значения поменяны местами.
 */
 
 export function invertMap<K, V>(map: Map<K, V>): Map<V, K> {
 	const newMap = new Map<V, K>();
+	const arrForMap: [V, K][]= [];
+
+	map.forEach((value, key) => arrForMap.push([value, key]))
+	arrForMap.forEach(elem => newMap.set(elem[0], elem[1]))
 
 	return newMap;
 }
